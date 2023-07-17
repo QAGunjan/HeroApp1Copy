1595d85bb31de8586498dab38844c225e9852f77
QAGunjan <127589071+QAGunjan@users.noreply.github.com>
2023-07-17 12:50:35 +0530

    Update Extent_Reports_Generator.java

--- src/main/java/com/oneapp/basic/Extent_Reports_Generator.java
+++ src/main/java/com/oneapp/basic/Extent_Reports_Generator.java
@@ -21,7 +21,7 @@ public class Extent_Reports_Generator {
 		
 //		String directory= "C:\\Users\\Lenovo\\.jenkins\\workspace\\Smoke_Suite\\Reports\\index.html ";
 //        String directory = "C:\\Users\\Lenovo\\.jenkins\\workspace\\SmokeSuiteTestCases\\Reports\\index.html";
-  String directory  ="C:\Users\Lenovo\.jenkins\workspace\SmokeSuiteTestCases\Reports";
+  String directory  ="C:/Users/Lenovo/.jenkins/workspace/SmokeSuiteTestCases/Reports/index.html";
 		//                String path= directory + fileName;
 		
 		String path= directory;
